073039717d1ca0ae9480ba0df59abe546f301489
SagarSalekar <salekarsagar123@gmail.com>
2017-06-25 19:23:20 +0530

    order summary added

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,16 +1,20 @@
 package com.example.android.justjava; /**
  * IMPORTANT: Add your package below. Package name can be found in the project's AndroidManifest.xml file.
  * This is the package name our example uses:
- *
+ * <p/>
  * package com.example.android.justjava;
- *
  */
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.text.NumberFormat;
 
@@ -19,56 +23,124 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 0;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
-    int quantity = 0;
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage="Toatal Rs. "+(quantity*5)+" \n Thank You";
-        displayMessage(priceMessage);
+        displayMessage(createOrderSummary("Sagar", calculatePrice(5)));
+    }
+
+    /**
+     * This method returns summary of an order
+     */
+    private String createOrderSummary(String name, int price) {
+        return ("Name: " + name + "\nQuantity: " + quantity + "\nTotal Rs." + price + " \nThank You!");
+    }
+
+    /**
+     * Calculates the price of the order.
+     *
+     * @param pricePerCup is the price per cups of coffee ordered
+     */
+    private int calculatePrice(int pricePerCup) {
+        return quantity * pricePerCup;
     }
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSumamryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSumamryTextView.setText(message);
     }
 
     /**
      * This method is called when the plus button is clicked.
      */
-    public void increment(View view){
-        quantity=quantity+1;
+    public void increment(View view) {
+        quantity = quantity + 1;
         display(quantity);
-        displayPrice(quantity * 5);
+        displayPrice(calculatePrice(5));
     }
+
     /**
      * This method is called when the minus button is clicked.
      */
-    public void decrement(View view){
-        quantity=quantity-1;
+    public void decrement(View view) {
+        quantity = quantity - 1;
         display(quantity);
-        displayPrice(quantity * 5);
+        displayPrice(calculatePrice(5));
     }
+
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void display(int numberOfCoffees) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numberOfCoffees);
     }
+
     /**
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.justjava/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.justjava/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
