96a93c1c8a0b1a287ddf4cb35d98da8f2d70a070
SagarSalekar <salekarsagar123@gmail.com>
2017-06-28 13:15:14 +0530

    added email intent

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -7,8 +7,11 @@ package com.example.android.justjava; /**
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -40,7 +43,14 @@ public class MainActivity extends AppCompatActivity {
     public void submitOrder(View view) {
         EditText nm = (EditText) findViewById(R.id.edit_text);
         String name = nm.getText().toString();
-        displayMessage(createOrderSummary(name, calculatePrice()));
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "coffee order");
+        intent.putExtra(Intent.EXTRA_TEXT, createOrderSummary(name, calculatePrice()));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+            Log.v("MainActivity", "email sent");
+        }
     }
 
     /**
@@ -91,13 +101,6 @@ public class MainActivity extends AppCompatActivity {
         return quantity * pricePerCup;
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSumamryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSumamryTextView.setText(message);
-    }
 
     /**
      * This method is called when the plus button is clicked.
